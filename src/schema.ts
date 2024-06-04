574fea73faf1e7169a44e912ed912fb97f84243e
iamdhariot <iamdhariot@gmail.com>
2024-06-04 07:54:56 +0530

    nexus added for typed GraphQL schema

---
+++ src/schema.ts
@@ -0,0 +1,14 @@
+import { makeSchema } from "nexus";
+import { join } from "path";
+export const schema = makeSchema({
+  /**
+   * Your GraphQL schema will consist of many types that you will pass as
+   * an array to the types object.
+   * For now, it is intentionally kept empty.
+   */
+  types: [],
+  outputs: {
+    schema: join(process.cwd(), "schema.graphql"), //Nexus will generate for you is a GraphQL schema file of type .graphql
+    typegen: join(process.cwd(), "nexus-typegen.ts"), //TypeScript file known as typegen. contain TypeScript type definitions for all types in your GraphQL schema
+  },
+});
