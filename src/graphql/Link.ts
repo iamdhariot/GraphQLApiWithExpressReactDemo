a92468cce905c1412af1e63ae0b0b4ba1a89afbe
iamdhariot <iamdhariot@gmail.com>
2024-06-04 12:03:38 +0530

    feed resolver added

--- src/graphql/Link.ts
+++ src/graphql/Link.ts
@@ -1,9 +1,39 @@
-import { objectType } from "nexus";
+import { extendType, objectType } from "nexus";
+import { NexusGenObjects } from "../../nexus-typegen";
 export const Link = objectType({
   name: "Link",
   definition(t) {
     t.nonNull.int("id");
     t.nonNull.string("description");
     t.nonNull.string("url");
+  },
+});
+let links: NexusGenObjects["Link"][] = [
+  {
+    id: 1,
+    url: "iamdhariot.vercel.app",
+    description: "Portfolio",
+  },
+  {
+    id: 2,
+    url: "iamdhariot.github.io",
+    description: "Portfolio",
+  },
+  {
+    id: 3,
+    url: "www.google.com",
+    description: "google",
+  },
+];
+
+export const LinkQuery = extendType({
+  type: "Query",
+  definition(t) {
+    t.nonNull.list.nonNull.field("feed", {
+      type: "Link",
+      resolve(parent, args, context, info) {
+        return links;
+      },
+    });
   },
 });
--- src/schema.ts
+++ src/schema.ts
@@ -1,12 +1,13 @@
 import { makeSchema } from "nexus";
 import { join } from "path";
+import * as types from "./graphql";
 export const schema = makeSchema({
   /**
    * Your GraphQL schema will consist of many types that you will pass as
    * an array to the types object.
    * For now, it is intentionally kept empty.
    */
-  types: [],
+  types,
   outputs: {
     schema: join(process.cwd(), "schema.graphql"), //Nexus will generate for you is a GraphQL schema file of type .graphql
     typegen: join(process.cwd(), "nexus-typegen.ts"), //TypeScript file known as typegen. contain TypeScript type definitions for all types in your GraphQL schema
